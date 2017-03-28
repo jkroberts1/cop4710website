ed95213d8dad588c6ad8a6c199a21b9817d543ad
SmilesBlue <SmilesBlue@users.noreply.github.com>
2017-03-27 23:08:06 -0400

    anotherone

---
+++ Project/RegisterEvent.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+$name = $_POST['name'];
+$category = $_POST['category'];
+$description = %_POST['description'];
+$time = %_POST['time'];
+$date = %_POST['date'];
+$location = %_POST['location'];
+$phone = %_POST['phone'];
+$email = %_POST['email'];
+try
+{
+  $reg_date = date("Y-m-d H:i:s");
+  $dbh = new PDO("mysql:host='localhost';dbname='cop4710'", 'root', 'root');
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $stmt = $dbh->prepare("INSERT INTO events (name, category, time, date, location, phone, email, reg_date) VALUES ($name, $category, $time, $date, $location, $phone, $email, $reg_date)");
+  $stmt->execute();
+  unset( $_SESSION['form_token'] );
+}
+catch(PDOException $e)
+{
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
+//name, eventcategory, description, time, date, location, contactphone, and contact emailaddress
