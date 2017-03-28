aa000c10a7e3062ad89aea349922893da5ca0064
SmilesBlue <SmilesBlue@users.noreply.github.com>
2017-03-27 22:55:23 -0400

    Fixes

--- Project/registerAccount.php
+++ Project/registerAccount.php
@@ -3,9 +3,11 @@ session_start();
 $username = $_POST['username'];
 $password = sha1($_POST['password']);
 $accountType = %_POST['accType'];
-try{
+try
+{
   $reg_date = date("Y-m-d H:i:s");
   $dbh = new PDO("mysql:host='localhost';dbname='cop4710'", 'root', 'root');
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $stmt = $dbh->prepare("INSERT INTO users (username, password, accountType, reg_date) VALUES (:username, :password, :accountType, :reg_date)");
   $stmt->bindParam(':username', $username, PDO::PARAM_STR);
   $stmt->bindParam(':password', $password, PDO::PARAM_STR, 40);
@@ -13,3 +15,8 @@ try{
   $stmt->execute();
   unset( $_SESSION['form_token'] );
 }
+catch(PDOException $e)
+{
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
