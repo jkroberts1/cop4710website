2733e68c1516a19d1d6b70b16c5b0e42b5b8ef07
SmilesBlue <SmilesBlue@users.noreply.github.com>
2017-03-27 22:30:53 -0400

    Should allow for accounts to be added to a database

--- Project/registerAccount.php
+++ Project/registerAccount.php
@@ -1,2 +1,15 @@
-
-//thing
+<?php
+session_start();
+$username = $_POST['username'];
+$password = sha1($_POST['password']);
+$accountType = %_POST['accType'];
+try{
+  $reg_date = date("Y-m-d H:i:s");
+  $dbh = new PDO("mysql:host='localhost';dbname='cop4710'", 'root', 'root');
+  $stmt = $dbh->prepare("INSERT INTO users (username, password, accountType, reg_date) VALUES (:username, :password, :accountType, :reg_date)");
+  $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+  $stmt->bindParam(':password', $password, PDO::PARAM_STR, 40);
+  $stmt->bindParam(':accountType', $accountType, PDO::PARAM_STR);
+  $stmt->execute();
+  unset( $_SESSION['form_token'] );
+}
