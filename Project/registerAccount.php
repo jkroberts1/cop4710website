41b07164703ff5b3bc37c8d5e51ffc80e54b0605
SmilesBlue <SmilesBlue@users.noreply.github.com>
2017-03-28 17:24:28 -0400

    Update registerAccount.php

--- Project/registerAccount.php
+++ Project/registerAccount.php
@@ -7,7 +7,7 @@ try
   $reg_date = date("Y-m-d H:i:s");
   $dbh = new PDO("mysql:host='localhost';dbname='cop4710'", 'root', 'root');
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $stmt = $dbh->prepare("INSERT INTO students (username, password, reg_date) VALUES (:username, :password, :reg_date)");
+  $stmt = $dbh->prepare("INSERT INTO students (username, psw, reg_date) VALUES (:username, :password, :reg_date)");
   $stmt->bindParam(':username', $username, PDO::PARAM_STR);
   $stmt->bindParam(':password', $password, PDO::PARAM_STR, 40);
   $stmt->bindParam(':reg_date', $reg_date, PDO::PARAM_STR);
